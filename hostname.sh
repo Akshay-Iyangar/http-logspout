4a2cf6c5d9ca6bb466ebae62acb0389f6ceabbc3
Akshay-Iyangar <Akshay.Iyangar@asu.edu>
2016-07-26 15:46:14 -0700

    adding buffer capacity for HttpAdapter

--- hostname.sh
+++ hostname.sh
@@ -5,7 +5,7 @@
 HOST_IP=$(curl http://rancher-metadata/2015-12-19/self/host/agent_ip)
 
 
-url=$(echo "http://$HOST_IP:12285?http.path=/v1/dc/logs/ecomm/logs")
+url=$(echo "http://$HOST_IP:12285?http.path=/v1/dc/logs/ecomm/logs\&http.buffer.capacity=9999")
 
 /bin/logspout $url
 
